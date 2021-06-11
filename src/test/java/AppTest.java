605475cfce7f49216f825b9a972a1ec88071b189
kolka4100 <81476202+kolka4100@users.noreply.github.com>
2021-06-11 13:01:15 +0300

    Initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class AppTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void mainTest() {
+        HelloWorld tree =
+                applicationContext.getBean(HelloWorld.class);
+        HelloWorld leaf =
+                applicationContext.getBean(HelloWorld.class);
+
+        Cat one = applicationContext.getBean(Cat.class);
+        Cat two = applicationContext.getBean(Cat.class);
+
+        boolean helloWorld = tree.equals(leaf);
+        boolean cat = one.equals(two);
+
+        System.out.println(helloWorld);
+        System.out.println(cat);
+        
+        Assert.assertSame("Тест провален, не корректная настройка бина HelloWorld", tree, leaf);
+        Assert.assertNotSame("Тест провален, не корректная настройка бина Cat", one, two);
+    }
+}
